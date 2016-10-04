6356fe60ec5cd510f5250b89bcbb91c41c1bd4e5
jaymspeights <jaymspeights@gmail.com>
2016-10-04 16:28:57 -0500

    Map now doesnt load and shows error is location isnt enabled. also cleaned up code

--- js/google_scripts.js
+++ js/google_scripts.js
@@ -1,5 +1,8 @@
 var loc = {latitude: 0, longitude: 0};
+var center = {lat: loc.latitude, lng: loc.longitude};
 var cityCircle;
+var map;
+var loopUpdate;
 
 function getLatitude()  {
   return loc.latitude;
@@ -8,31 +11,55 @@ function getLongitude() {
   return loc.longitude;
 }
 
-function updateLocation() {
+function initMap() {
     if (navigator && navigator.geolocation) {
-        console.log('map updated');
-
-        return navigator.geolocation.getCurrentPosition(success, ()=>console.log('Geolocation error'));
+        console.log('Geolocation is supported');
+        return navigator.geolocation.getCurrentPosition(locationFound, failure);
     } else {
         console.log('Geolocation is not supported');
     }
 
 }
 
-function success(position)	{
-loc = position.coords;
-var center = {lat: loc.latitude, lng: loc.longitude};
-if (cityCircle != null) cityCircle.setPosition(center);
+function updateLocation() {
+    if (navigator && navigator.geolocation) {
+        return navigator.geolocation.getCurrentPosition(success, ()=>console.log('location not updated'));
+    } else {
+        console.log('location update failed');
+    }
 
-else {
-cityCircle = new google.maps.Marker({
-          position: center,
-          icon: {
-            url: "sprites/dick.gif",
-            scale: 7
-          },
-          draggable: true,
-          map: map
-        });
-      }
+}
+
+function locationFound(position){
+  loc = position.coords;
+  center = {lat: loc.latitude, lng: loc.longitude};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: center,
+    zoom: 13
+  });
+  console.log('map initialized');
+  console.log('location found');
+  cityCircle = new google.maps.Marker({
+            position: center,
+            icon: {
+              url: "sprites/dick.gif",
+              scale: 9,
+              anchor: {x: 41, y: 32}
+            },
+            map: map
+          });
+  loopUpdate = window.setInterval(updateLocation, 1000);
+}
+
+
+function failure(){
+  console.log('Geolocation error');
+  document.getElementById("map").innerHTML="Location services are not enabled"
+}
+
+function success(position)	{
+  console.log('location updated');
+  loc = position.coords;
+  center = {lat: loc.latitude, lng: loc.longitude};
+  cityCircle.setPosition(center);
 }
